153d2676d0fb5f813755410c89ce3508f0986062
Maksym Martynenko <predatar24@gmail.com>
2023-01-25 19:38:25 +0200

    add swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,46 @@
+import Swiper from 'swiper';
+
 window.addEventListener('DOMContentLoaded', () => {
     const footerArrow = document.querySelectorAll('.footer__arrow'),
         footerList = document.querySelectorAll('.footer__list'),
-        timer_h = document.querySelector('.profile-info__timer-hours span'),
-        timer_m = document.querySelector('.profile-info__timer-minutes span'),
-        timer_s = document.querySelector('.profile-info__timer-seconds span');
+        timer_h = document.querySelectorAll('.profile-info__timer-hours span'),
+        timer_m = document.querySelectorAll('.profile-info__timer-minutes span'),
+        timer_s = document.querySelectorAll('.profile-info__timer-seconds span');
+    const deadline = new Date('2023', '01', '27');
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        slidesPerView: 3,
+        enabled: false,
+        spaceBetween: 35,
+        breakpoints: {
+            375: {
+                slidesPerView: 1,
+                enabled: true
+            },
+            426: {
+                slidesPerView: 1,
+                enabled: true
+            },
+            576: {
+                slidesPerView: 1,
+                enabled: true
+            },
+            769: {
+                slidesPerView: 2,
+                enabled: true,
+                grabCursor: true
+            },
+            1025: {
+                slidesPerView: 2,
+                enabled: true,
+                grabCursor: true
+            },
+            1436: {
+                slidesPerView: 3,
+                enabled: false
+            }
+        }
+    });
 
     footerArrow.forEach((elem, i) => {
         elem.addEventListener('click', () => {
@@ -11,8 +48,6 @@ window.addEventListener('DOMContentLoaded', () => {
             footerList[i].classList.toggle('show');
         });
     });
-
-    const deadline = new Date('2023', '01', '25');
 
     function countDownTimer() {
         const diff = deadline - new Date();
@@ -24,9 +59,15 @@ window.addEventListener('DOMContentLoaded', () => {
         const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
         const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
 
-        timer_h.textContent = hours < 10 ? '0' + hours : hours;
-        timer_m.textContent = minutes < 10 ? '0' + minutes : minutes;
-        timer_s.textContent = seconds < 10 ? '0' + seconds : seconds;
+        timer_h.forEach(elem => {
+            elem.textContent = hours < 10 ? '0' + hours : hours;
+        });
+        timer_m.forEach(elem => {
+            elem.textContent = minutes < 10 ? '0' + minutes : minutes;
+        });
+        timer_s.forEach(elem => {
+            elem.textContent = seconds < 10 ? '0' + seconds : seconds;
+        });
     }
 
     const timerId = setInterval(countDownTimer, 1000);
